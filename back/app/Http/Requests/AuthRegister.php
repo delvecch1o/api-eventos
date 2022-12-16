26d2761d11dbafbd7cc76d9aba80de9882956238
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-12-16 18:46:12 -0300

    validator cpf

--- back/app/Http/Requests/AuthRegister.php
+++ back/app/Http/Requests/AuthRegister.php
@@ -26,7 +26,7 @@ class AuthRegister extends FormRequest
         return [
             'name' => 'required|max:191',
             'email' => 'required|email|max:191|unique:users,email',
-            'cpf' => 'required|unique:users,cpf',
+            'cpf' => 'required|max:11|unique:users,cpf',
             'password' => 'required|string|min:8|confirmed',
             'password_confirmation' => 'required',
             'role' => 'required|max:191',
@@ -40,6 +40,7 @@ class AuthRegister extends FormRequest
            'email.unique' => 'O email ja foi cadastrado',
            'cpf.required' => 'O cpf é obrigatório',
            'cpf.unique' => 'O cpf ja foi cadastrado',
+           'cpf.max' => 'CPF invalido, maximo 11 numeros',
            'password.required' => 'A senha é obrigatório',
            'password.confirmed' => 'As senhas não são iguais',
            'password.min' => 'Senha muito curta, mínimo 8 caracteres',
