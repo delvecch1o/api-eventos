9d4132fd892d0682651bc10ae27114fc23f72f5d
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-12-21 21:25:13 -0300

    front

---
+++ front/src/Routes/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import axios from 'axios';
+
+
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Switch>
+    
+                    
+                    
+                    
+    
+                </Switch>
+            </Router>
+        </div>
+    
+    )
+    }
+    
+    export default App;
