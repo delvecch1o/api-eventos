9e21a324eb4ffebe58d9b04b6e27d5a21775347f
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-12-22 07:32:10 -0300

    criar eventos

--- front/src/Pages/Event/index.js
+++ front/src/Pages/Event/index.js
@@ -1,13 +1,111 @@
-import React from "react";
-import Header from '../../Components/Header/index'
+import React, { useState } from "react";
+import Header from '../../Components/Header/index';
+import axios from 'axios';
+import { Container, Form, Label, Input, LabelError, Button } from './styles';
+
 
 function Event() {
-    return (
-        <div>
-            <Header />
-            <h1>Tela Eventos</h1>
 
-        </div>
+    const [nome, setNome] = useState();
+    const [palestrante, setPalestrante] = useState();
+    const [inicio, setInicio] = useState();
+    const [fim, setFim] = useState();
+    const [descricao, setDescricao] = useState();
+    const [participantes, setParticipantes] = useState();
+    const [error, setError] = useState();
+
+    const submitEvent = (e) => {
+        e.preventDefault();
+        if (!nome | !palestrante | !inicio | !fim | !descricao | !participantes) {
+            setError("Preencha todos os campos");
+            return;
+        }
+
+        const data = {
+            nome: nome,
+            palestrante: palestrante,
+            inicio: inicio,
+            fim: fim,
+            descrição: descricao,
+            numero_de_participantes: participantes
+        }
+
+        axios.get('/sanctum/csrf-cookie').then(response => {
+            axios.post('/api/event', data)
+                .then(res => {
+
+                    alert("Evento criado com Sucesso!")
+
+                })
+                .catch((error) => {
+                    alert("Erro \n" + error.response.data.errors.fim);
+
+
+                });
+        });
+
+
+    }
+
+
+    return (
+        <>
+            <Header />
+            <Container>
+                <Label>Crie um Evento</Label>
+                <Form onSubmit={submitEvent} >
+                    <Input
+                        type='text'
+                        placeholder='Nome do Evento'
+                        value={nome}
+                        onChange={(e) => [setNome(e.target.value), setError("")]}
+
+                    />
+                    <Input
+                        type='text'
+                        placeholder='Palestrante'
+                        value={palestrante}
+                        onChange={(e) => [setPalestrante(e.target.value), setError("")]}
+
+                    />
+
+                    <Input
+                        type='date'
+                        value={inicio}
+                        onChange={(e) => [setInicio(e.target.value), setError("")]}
+
+
+                    />
+
+                    <Input
+                        type="date"
+                        value={fim}
+                        onChange={(e) => [setFim(e.target.value), setError("")]}
+
+
+
+                    />
+                    <Input
+                        type='text'
+                        placeholder='Descrição'
+                        value={descricao}
+                        onChange={(e) => [setDescricao(e.target.value), setError("")]}
+
+                    />
+                    <Input
+                        type='number'
+                        placeholder='Numero de Participantes'
+                        value={participantes}
+                        onChange={(e) => [setParticipantes(e.target.value), setError("")]}
+
+                    />
+                    <LabelError>{error}</LabelError>
+                    <Button type='submit'>Criar Evento</Button>
+
+                </Form>
+            </Container>
+
+        </>
 
     )
 }
